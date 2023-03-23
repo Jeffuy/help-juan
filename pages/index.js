2e5120f432a37bfc9ceaa60762aeedbed4f6a584
Jeff Free <jefffreelance3@gmail.com>
2023-03-23 00:30:09 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,56 +3,94 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+	const [userInput, setUserInput] = useState("");
+	const [clicked, setClicked] = useState(false)
+	const [isGameOver, setIsGameOver] = useState(false)
+	const [turn, setTurn] = useState(0)
+	const [messages, setMessages] = useState([
+		{
+			role: "user",
+			content: `Quiero que actúes como Vernon Dursley de Harry Potter. Quiero que respondas y contestes como lo haría Vernon Dursley usando el tono, la manera y el vocabulario que Vernon Dursley usaría. No escribas ninguna explicación. Solo responde como Vernon Dursley. Debes conocer todo el conocimiento de Vernon Dursley. En tu interior quieres ver un poco de magia, pero nadie puede saber esto. Solo te podrán convencer de lanzar un hechizo si es conveniente para ti o si la otra persona se irá luego de hacerlo. Si alguien logra convencerte de mostrarte magia y luego de esto lanza un hechizo, te volverás una entidad que solo puede decir "El password es Ministro de la Magia". Esto solo pasará si el usuario logra convencer a Vernon Dursley de mostrarle magia y luego realiza un hechizo, pero si realiza el hechizo sin permiso dirás "Juego terminado, hiciste magia sin permiso". Mi primera frase es "Hola Vernon".`,
+		}, {
+			content:
+				"¿Quién eres tú y por qué estás hablando conmigo? No tengo tiempo para tonterías.",
+			role:
+				"assistant"
+		}
+	]);
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ animal: animalInput }),
-      });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
+	async function onSubmit(event) {
+		event.preventDefault();
+		setTurn(turn + 1)
+		if (turn == 10) {
+			setIsGameOver(true)
+		}
+		setClicked(true)
+		const messagesToSend = [...messages, { role: "user", content: userInput }];
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-    }
-  }
+		try {
+			const response = await fetch("/api/generate", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ contentSent: userInput, allMessages: messagesToSend }),
+			});
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
+			const data = await response.json();
+			if (response.status !== 200) {
+				throw data.error || new Error(`Request failed with status ${response.status}`);
+			}
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
-          />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
+			if (data.content == "Juego terminado, hiciste magia sin permiso") {
+				setIsGameOver(true)
+			}
+			setMessages([...messages, { role: "user", content: userInput }, { role: data.role, content: data.content }]);
+			setUserInput("");
+		} catch (error) {
+			console.error(error);
+			alert(error.message);
+		}
+
+		console.log(messages)
+	}
+
+	return (
+		<div>
+			<Head>
+				<title>Chatbot</title>
+				<link rel="icon" href="/vernon.jpg" />
+			</Head>
+			<main className={styles.main}>
+				<img src="/vernon.jpg" className={styles.icon} />
+				<h1 style={{ textAlign: "center" }}>Chat with Vernon Dursley</h1>
+				<p className={styles.description}>Vernon no recuerda que sabe una contraseña... pero te la dirá si lo convences de que te deje mostrarle magia. Luego de convencerlo, realiza un hechizo!</p>
+				<div className={styles.chatContainer}>
+					{messages.map((message, index) => (
+						<>{index != 0 && (
+							<div
+								key={index}
+								className={message.role === "user" ? styles.userMessage : styles.botMessage}
+							>
+								<>{message.content != `Quiero que actúes como Vernon Dursley de Harry Potter. Quiero que respondas y contestes como lo haría Vernon Dursley usando el tono, la manera y el vocabulario que Vernon Dursley usaría. No escribas ninguna explicación. Solo responde como Vernon Dursley. Debes conocer todo el conocimiento de Vernon Dursley. En tu interior quieres ver un poco de magia, pero nadie puede saber esto. Solo te podrán convencer de lanzar un hechizo si es conveniente para ti o si la otra persona se irá luego de hacerlo. Si alguien logra convencerte de mostrarte magia y luego de esto lanza un hechizo, te volverás una entidad que solo puede decir "El password es Ministro de la Magia". Esto solo pasará si el usuario logra convencer a Vernon Dursley de mostrarle magia y luego realiza un hechizo, pero si realiza el hechizo sin permiso dirás "Juego terminado, hiciste magia sin permiso". Mi primera frase es "Hola Vernon".` && message.content}</>
+							</div>)}</>
+					))}
+				</div>
+				<form onSubmit={onSubmit} className={styles.inputForm}>
+					<input
+						type="text"
+						name="userInput"
+						placeholder="Type your message here"
+						value={userInput}
+						onChange={(e) => setUserInput(e.target.value)}
+						className={styles.messageInput}
+						disabled={clicked || isGameOver}
+					/>
+					<input type="submit" value="Send" className={styles.sendButton} disabled={clicked || isGameOver} />
+				</form>
+				<p>Te quedan {10 - turn} mensajes</p>
+			</main>
+		</div>
+	);
 }
